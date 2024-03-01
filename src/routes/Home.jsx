a6e86512477db6431ac4340fe8b0c9835be94163
Inna Kozhina <inna.kozhina22@gmail.com>
2024-03-01 13:51:11 +0200

    set useContext

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,12 +1,39 @@
-import { Typography } from "@mui/material"
+import { useState } from "react";
+import PlayerControls from "../components/PlayerControls";
+import TrackList from "../components/TackList";
+import sinisterMusic from "../assets/sinister.mp3";
+import underwaterMusic from "../assets/underwater.mp3";
+import stompingMusic from "../assets/stomp.mp3";
+import { MusicContext } from "../contexts/MusicContext";
 
 const Home = () => {
+  const [state, setState] = useState({
+    audioPlayer: new Audio(),
+    tracks: [
+      {
+        name: "Sinister Music",
+        file: sinisterMusic,
+      },
+      {
+        name: "Stomping music",
+        file: stompingMusic,
+      },
+      {
+        name: "Underwater Music",
+        file: underwaterMusic,
+      },
+    ],
+    currentTrackIndex: 0,
+    isPlaying: false,
+  });
+  return (
+    <div>
+      <MusicContext.Provider value={[state, setState]}>
+        <PlayerControls />
+        <TrackList />
+      </MusicContext.Provider>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <Typography variant="h6" component="h2">Home</Typography>
-        </div>
-    )
-}
-
-export default Home
+export default Home;
